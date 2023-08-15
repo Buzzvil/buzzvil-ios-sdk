7cd42b79279517b26b0bed894a7b1cff4c841dee
CircleCI Job <buzzvil-client-ci@buzzvil.com>
2023-08-15 14:04:37 +0000

    Update SDK to 1.0.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,5 @@
 // swift-tools-version: 5.7
-
 import PackageDescription
-
 let package = Package(
   name: "buzzvil-ios-sdk",
   platforms: [
@@ -27,8 +25,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "BuzzBoosterSDK",
-      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/8220/BuzzBoosterSDK.zip",
-      checksum: "52f65c4e644c928f72cce065445cbbad6113224fbf5b10f44746b7140885c1c9"
+      url: "https://buzzvil.com",
+      checksum: "123123"
     ),
     .binaryTarget(
       name: "BuzzRxSwift",
