57e42582c0e90dd9295353734339bbe516f78fa9
Daniel Song <68672528+Minkwan-Song@users.noreply.github.com>
2026-02-13 15:00:09 +0900

    6.6.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-ios", .upToNextMajor(from: "3.23.0")),
   ],
   targets: [
     .target(
@@ -24,31 +23,18 @@ let package = Package(
       dependencies: [
         "BuzzvilSDK",
         "BuzzAdBenefitSDK",
-        "BuzzBoosterSDK",
-        "BuzzRxSwift",
-        .product(name: "GoogleInteractiveMediaAds", package: "swift-package-manager-google-interactive-media-ads-ios"),
       ],
       path: "Sources/BuzzvilSDKTarget"
     ),
     .binaryTarget(
       name: "BuzzvilSDK",
-      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/60501-60/BuzzvilSDK.zip",
-      checksum: "78f1066b2fe20ef3fee01fa0baf7f269560a0fcfadf66acece01e123eef13f71"
+      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/60600-77/BuzzvilSDK.zip",
+      checksum: "fc269dd089139040c820705f0dd6143fb46249936ede7732ba6d89a868691bc9"
     ),
     .binaryTarget(
       name: "BuzzAdBenefitSDK",
-      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/60501-60/BuzzAdBenefitSDK.zip",
-      checksum: "be4e1cca5d47dea3feba5559b85e2b06b0745bcaa8ef763bdb6324b5b8f08ad0"
-    ),
-    .binaryTarget(
-      name: "BuzzBoosterSDK",
-      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/60501-60/BuzzBoosterSDK.zip",
-      checksum: "9d75c256cc45ba5f740e321bba0e59873706eaebd5c44cd1272973d67614a120"
-    ),
-    .binaryTarget(
-      name: "BuzzRxSwift",
-      url: "https://github.com/Buzzvil/RxSwift/releases/download/v7.0.0/BuzzRxSwift.zip",
-      checksum: "772edfb6e77b41f888d4430e46e00d1ec147dc9d26b3f2ce464dbc00d678f963"
+      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/60600-77/BuzzAdBenefitSDK.zip",
+      checksum: "9bb3553fa8994d3abadd467eba101e477461b231db6a3fb21d4326b579a16c9f"
     ),
   ],
   swiftLanguageVersions: [
